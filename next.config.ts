85ab0d4e61d7e0c231aeb07e37642a1ef06032a9
David Bradford <davidbradforduko@yahoo.com>
2026-09-05 09:16:25 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'i.pravatar.cc',
       },
+      {
+        protocol: 'https',
+        hostname: '*.public.blob.vercel-storage.com',
+      },
     ],
     localPatterns: [
       {
