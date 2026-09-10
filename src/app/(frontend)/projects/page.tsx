fdd4aebe1f02167bd803b873ad4fa92ea83e0f48
David Bradford <davidbradforduko@yahoo.com>
2026-09-10 14:29:52 +0100

    revalidate pages

--- src/app/(frontend)/projects/page.tsx
+++ src/app/(frontend)/projects/page.tsx
@@ -2,6 +2,8 @@ import RecentProjects from '@/components/RecentProjects'
 import ManagedProjects from '@/components/ManagedProjects'
 import ProjectsHero from '@/components/ProjectsHero'
 
+export const revalidate = 60 // Revalidate every 60 seconds
+
 export default function ProjectsPage() {
   return (
     <div className="min-h-screen">
